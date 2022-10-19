315ee447ba37d2eb7c9ac3daeb25b233bd028842
Mathi4s <mathiasmesta2003@gmail.com>
2022-10-19 12:17:48 -0500

    api-AUTORES

---
+++ back/API-autores/API-autores/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using API_autores.Entitys;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_autores
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Configurando las tablas de las bases de datos
+        public DbSet<Autor> Autor { get; set; }
+    }
+}
---
+++ back/API-autores/API-autores/Entitys/Autor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_autores.Entitys
+{
+    public class Autor
+    {
+        //Definimos clave primaria
+        [Key]
+        public int codigo { get; set;}
+        //Definimos valores no nulos
+        [Required]
+        //Definimos el tamaño del campo
+        [StringLength(maximumLength:100, ErrorMessage ="Se tiene que ingresar un nombre")]
+        public string nombre { get; set;}
+        [Required]
+        public bool estado { get; set;}
+    }
+}
